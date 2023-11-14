67c939bb25f6f6def8787b1ac03c9570b8594803
BeniAS104 <beniuu104@gmail.com>
2023-11-14 19:07:45 +0100

    aaaa
    aaaa

--- Vote Button/vbutton.js
+++ Vote Button/vbutton.js
@@ -17,13 +17,3 @@ function playsound() {
     const timestampContainer = document.getElementById('timestamp-container2');
     timestampContainer.innerHTML = `${timestamp}`;
 }
-
-// Helper function to get a random color
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
