47b85469d2d51ac319993a5b19186d48f0c66fa4
BeniAS104 <beniuu104@gmail.com>
2023-11-14 18:59:22 +0100

    aa
    aa

--- Vote Button/vbutton.js
+++ Vote Button/vbutton.js
@@ -1,41 +1,24 @@
-//nonoverlapping sounds
-// function playsound() {
-//     const funnysound = document.getElementById('funnysound');
-//     funnysound.play();
-//   }
 
-// randomsounds
-// function playsound() {
-//     const randomSound = Math.random() < 0.5 ? 'Images/SUIII.mp3' : 'path/to/second-sound.mp3';
-//     const audioElement = new Audio(randomSound);
-//     audioElement.play();
-// }
-
-// random sounds with flashing border
 let soundCounter = 0;
-let originalBorderStyle; // Variable to store the original border style
 
 function playsound() {
     const sounds = ['SUIII.mp3', 'jixaw-metal-pipe-falling-sound.mp3', 'minecraft-train-whistle-cave-sound.mp3', 'bruh.mp3'];
     const audioElement = new Audio(sounds[soundCounter]);
     audioElement.play();
 
-    // Set an initial border style, color, and width if not already set
-    document.body.style.border = document.body.style.border || '1px solid';
-    document.body.style.borderColor = document.body.style.borderColor || getRandomColor();
-    originalBorderStyle = document.body.style.border;
-
-    // Change the border color
-    document.body.style.border = '30px solid ' + getRandomColor();
-
-    // Reset the border style and color after 0.5 seconds
-    setTimeout(() => {
-        document.body.style.border = document.body.style.border = '30px solid #0c1022';
-        ;
-    }, 500);
 
     // Increment the counter for the next button press
     soundCounter = (soundCounter + 1) % sounds.length;
+
+    const now = new Date();
+
+    const timestamp = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.${now.getMilliseconds()}`;
+
+    
+
+    // Display the timestamp in the designated container
+    const timestampContainer = document.getElementById('timestamp-container2');
+    timestampContainer.innerHTML = `${timestamp}`;
 }
 
 // Helper function to get a random color
